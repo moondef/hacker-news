b86845b7561ad5cf2d1676f5c8b0d76938483025
Stepan Samko <step.samko@gmail.com>
2018-04-08 16:49:38 +0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,156 +1,16 @@
 const rp = require("request-promise");
 const baseUrl = "https://hacker-news.firebaseio.com/v0/";
 
-module.exports.getPopular = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/topstories.json",
-      json: true
-    });
+const get = url => rp({ baseUrl, url, json: true });
 
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getUser = async nickname => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: `/user/${nickname}.json`,
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getItem = async itemID => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: `/item/${itemID}.json`,
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getMaxItem = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/maxitem.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getAskStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/askstories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getShowStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/showstories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getJobStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/jobstories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getUpdates = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/updates.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getNewStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/newstories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getTopStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/topstories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
-
-module.exports.getBestStories = async () => {
-  try {
-    const response = rp({
-      baseUrl,
-      url: "/beststories.json",
-      json: true
-    });
-
-    return response;
-  } catch (err) {
-    throw err;
-  }
-};
+module.exports.getPopular = () => get("/topstories.json");
+module.exports.getUser = nickname => get(`/user/${nickname}.json`);
+module.exports.getItem = itemID => get(`/item/${itemID}.json`);
+module.exports.getMaxItem = () => get("/maxitem.json");
+module.exports.getAskStories = () => get("/askstories.json");
+module.exports.getShowStories = () => get("/showstories.json");
+module.exports.getJobStories = () => get("/jobstories.json");
+module.exports.getUpdates = () => get("/updates.json");
+module.exports.getNewStories = () => get("/newstories.json");
+module.exports.getTopStories = () => get("/topstories.json");
+module.exports.getBestStories = () => get("/beststories.json");
